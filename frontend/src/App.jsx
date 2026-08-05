d893c96503e3abed5f1266e04e9e631fd5522105
aochuka <ochukaamon@gmail.com>
2026-08-05 12:36:56 +0300

    Add React Router for page navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+
 import SearchPage from "./pages/SearchPage";
+import AdminPage from "./pages/AdminPage";
 
 function App() {
-  return <SearchPage />;
+  return (
+    <Routes>
+      <Route path="/" element={<SearchPage />} />
+      <Route path="/admin" element={<AdminPage />} />
+    </Routes>
+  );
 }
 
 export default App;
